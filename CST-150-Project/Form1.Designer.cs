25fe4a0e68619580826719c9754f867b3d30aa2b
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-08-14 16:52:47 -0400

    Add project files.

---
+++ CST-150-Project/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CST_150_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstbx_inventory = new System.Windows.Forms.ListBox();
+            this.tbx_search = new System.Windows.Forms.TextBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_current_stock = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_delete_item = new System.Windows.Forms.Button();
+            this.lbl_title = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstbx_inventory
+            // 
+            this.lstbx_inventory.FormattingEnabled = true;
+            this.lstbx_inventory.Location = new System.Drawing.Point(270, 121);
+            this.lstbx_inventory.Name = "lstbx_inventory";
+            this.lstbx_inventory.Size = new System.Drawing.Size(238, 147);
+            this.lstbx_inventory.TabIndex = 1;
+            // 
+            // tbx_search
+            // 
+            this.tbx_search.Location = new System.Drawing.Point(335, 76);
+            this.tbx_search.Name = "tbx_search";
+            this.tbx_search.Size = new System.Drawing.Size(100, 20);
+            this.tbx_search.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(403, 386);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(273, 373);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(211, 373);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Item name";
+            // 
+            // lbl_current_stock
+            // 
+            this.lbl_current_stock.AutoSize = true;
+            this.lbl_current_stock.Location = new System.Drawing.Point(332, 282);
+            this.lbl_current_stock.Name = "lbl_current_stock";
+            this.lbl_current_stock.Size = new System.Drawing.Size(76, 13);
+            this.lbl_current_stock.TabIndex = 9;
+            this.lbl_current_stock.Text = "Current stock: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(438, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Stock";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(273, 399);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(211, 402);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Item type";
+            // 
+            // btn_delete_item
+            // 
+            this.btn_delete_item.Location = new System.Drawing.Point(565, 178);
+            this.btn_delete_item.Name = "btn_delete_item";
+            this.btn_delete_item.Size = new System.Drawing.Size(75, 23);
+            this.btn_delete_item.TabIndex = 13;
+            this.btn_delete_item.Text = "Delete item";
+            this.btn_delete_item.UseVisualStyleBackColor = true;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Location = new System.Drawing.Point(330, 27);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(116, 13);
+            this.lbl_title.TabIndex = 14;
+            this.lbl_title.Text = "Inventory Management";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 539);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.btn_delete_item);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_current_stock);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.tbx_search);
+            this.Controls.Add(this.lstbx_inventory);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstbx_inventory;
+        private System.Windows.Forms.TextBox tbx_search;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_current_stock;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_delete_item;
+        private System.Windows.Forms.Label lbl_title;
+    }
+}
+
---
+++ CST-150-Project/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CST_150_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class Item
+        {
+            public string itemName;
+            public int qty;
+
+            public Item(string a, int b)
+            {
+                itemName = a;
+                qty = b;
+            }
+        }
+
+        class Footwear : Item
+        {
+            public string type;
+            public double size;
+
+            public Footwear (string a, int b, string c, double d) : base (a, b)
+            {
+                type = c;
+                size = d;
+            }
+        }
+
+        class Softgoods : Item
+        {
+            public string type;
+            public double size;
+
+            public Softgoods (string a, int b, string c, double d) : base (a, b)
+            {
+                type = c;
+                size = d;
+            }
+        }
+
+        class Hardgoods : Item
+        {
+            public string type;
+            public double size;
+
+            public Hardgoods (string a, int b, string c, double d) : base (a, b)
+            {
+                type = c;
+                size = d;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Footwear shoe = new Footwear("shoe", 16, "north face", 9.5);
+            Footwear boot = new Footwear("boot", 13, "lowa", 10);
+            Hardgoods tent = new Hardgoods("tent", 7, "3 season", 2);
+
+            lstbx_inventory.Items.Add(shoe.itemName);
+            lstbx_inventory.Items.Add(boot.itemName);
+            lstbx_inventory.Items.Add(tent.type);
+
+            List<string> inventoryList = new List<string>();
+        }
+    }
+}
